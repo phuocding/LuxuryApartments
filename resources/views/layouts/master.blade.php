83ba79714ec03b3388f65de7f3fd8a9ba679a9d5
phuocding <phuocding@gmail.com>
2018-08-05 01:09:06 +0700

    Fix navigation bar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,11 +18,13 @@
     </nav>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="col-md-7 collapse navbar-collapse">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <ul class="navbar-nav">
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarNavDropdown"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
@@ -39,8 +41,7 @@
                     <a class="nav-link" href="#">Contact</a>
                 </li>
             </ul>
-        </div>
-        <div class="col-md-5">
+
             <form class="row">
                 <div class="col-md-8">
                     <input class="form-control" type="search" placeholder="Search" aria-label="Search">
